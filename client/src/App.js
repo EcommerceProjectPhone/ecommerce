1bdba204c8bc70a731d2554a46527e7dcaf61152
medaliabid <dali.abid2014@gmail.com>
2023-07-11 23:39:37 +0100

    changed

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 import {Routes,Route } from "react-router-dom"
 import './App.css';
 import Home from "./component/Home.jsx"
-import Login from "./component/Login.JSX";
+import Login from "./component/Login.jsx";
 
 const App = () => {
 
