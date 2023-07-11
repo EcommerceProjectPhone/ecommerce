e5b8d898bbb84b6525acacd49d49cb5610a019fc
walachafay <wala.ess2002@gmail.com>
2023-07-11 23:12:58 +0100

    nav

---
+++ client/src/component/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from "../../src/component/Navbar.jsx"
+import Product from '../../src/component/Product.jsx'
+const Home = () => {
+  return (
+    <div>
+      <Navbar/>
+<Product/>
+
+<h4 className='highest '>Highest Quality
+Collection</h4>
+<h6>Get Started</h6>
+
+
+    </div>
+  )
+}
+
+export default Home
