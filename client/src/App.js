4c50bf779ccdfdf4483e8ad893dea9283803985a
medaliabid <dali.abid2014@gmail.com>
2023-07-12 10:57:25 +0100

    2 components

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import React from "react";
 import {Routes,Route } from "react-router-dom"
 import './App.css';
 import Home from "./component/Home.jsx"
+import Login  from "./component/Login.jsx";
+import Signup from "./component/Signup.jsx";
 
 
 
@@ -12,8 +14,8 @@ const App = () => {
     <> 
     <Routes>
     <Route path='/'  element={<Home/>}/>
-
- 
+    <Route path='/login'  element={<Login/>}/>
+    <Route path='/Signup'  element={<Signup/>}/>
     </Routes>
     </>
 
