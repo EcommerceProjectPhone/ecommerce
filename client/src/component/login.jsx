bcb07c62c5955175bf81437af792f1bbabad6b55
medaliabid <dali.abid2014@gmail.com>
2023-07-13 12:33:59 +0100

    related to back

--- client/src/component/login.jsx
+++ client/src/component/login.jsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Grid, Paper, Avatar, Typography, TextField, Button } from '@material-ui/core';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
-import './Login.css'
+import axios from 'axios';
 
-const Login = () => {
+const Login = ({ handleLogin }) => {
+  const history = useHistory();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   const paperStyle = { padding: 20, height: '70vh', width: 280, margin: '20px auto' };
-  const avatarStyle = { backgroundColor: '#1bbd7e' };
+  const avatarStyle = { backgroundColor: '#1bbd7e'};
   const btnstyle = { margin: '8px 0' };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('/users/login', { email, password });
+      const token = response.data.token;
+      handleLogin(token);
+      history.push('/home');
+    } catch (error) {
+      console.error('Login Error:', error);
+    }
+  };
 
   return (
     <Grid>
@@ -20,21 +36,38 @@ const Login = () => {
           </Avatar>
           <h2>Sign In</h2>
         </Grid>
-        <TextField label="Username" placeholder="Enter username" fullWidth required />
-        <TextField label="Password" placeholder="Enter password" type="password" fullWidth required />
-        <FormControlLabel
-          control={<Checkbox name="checkedB" color="primary" />}
-          label="Remember me"
-        />
-        <Button type="submit" color="primary" variant="contained" style={btnstyle} fullWidth>
-          Sign in
-        </Button>
-        <Typography>
-          <Link to="/signup">Forgot password?</Link>
-        </Typography>
-        <Typography>
-          Do you have an account? <Link to="/signup">Sign Up</Link>
-        </Typography>
+        <form onSubmit={handleSubmit}>
+          <TextField
+            label="Email"
+            placeholder="Enter email"
+            fullWidth
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <TextField
+            label="Password"
+            placeholder="Enter password"
+            type="password"
+            fullWidth
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <FormControlLabel
+            control={<Checkbox name="checkedB" color="primary" />}
+            label="Remember me"
+          />
+          <Button type="submit" color="primary" variant="contained" style={btnstyle} fullWidth>
+            Sign in
+          </Button>
+          <Typography>
+            <Link to="/signup">Forgot password?</Link>
+          </Typography>
+          <Typography>
+            Do you have an account? <Link to="/signup">Sign Up</Link>
+          </Typography>
+        </form>
       </Paper>
     </Grid>
   );
