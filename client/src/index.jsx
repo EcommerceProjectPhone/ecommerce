e0ff2d6669c52fd2701544cda0a1d13ab256b4fa
MoNourTrabelsi <trabelsimednour@gmail.com>
2023-07-14 09:09:35 +0100

    test

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,24 +4,52 @@ import $ from 'jquery'
 import axios from 'axios'
 import Profile from './components/profile.jsx'
 import Posts from './components/post.jsx'
+import Photos from './components/photos.jsx'
 const App = (id) => {
     const [user, setUser] = useState([])
+    const [state, setState] = useState(false)
+    const [file, setFile] = useState("")
+    const [change, setChange] = useState(false)
+    const [updated, setUpdated] = useState(false)
 
     useEffect(() => {
         axios.get(`http://127.0.0.1:3000/api/profile/get/1`)
             .then((res) => { setUser(res.data) })
             .catch((err) => console.log(err))
-    }, [])
+    }, [updated,state])
 
-    const editProfile = (id) => {
-        axios.patch(`http://127.0.0.1:3000/api/profile/edit/${id}`)
-            .then((res) => alert("changed successfully"))
-            .catch((err) => console.log(err))
+    const changeProfile = async (id) => {
+        const form = new FormData()
+        form.append("file", file)
+        form.append("upload_preset", "blogging")
+        const res = await axios.post("https://api.cloudinary.com/v1_1/dx3tuofza/upload", form)
+        const url = res.data.secure_url
+        await axios.patch(`http://127.0.0.1:3000/api/profile/main/${id}`, {
+            url:url
+        })
+        .then((res) => { setState(!state) })
+        .catch((err) => alert("failed to post blog"))
     }
+
+    const changeCover = async (id) => {
+        const form = new FormData()
+        form.append("file", file)
+        form.append("upload_preset", "blogging")
+        const res = await axios.post("https://api.cloudinary.com/v1_1/dx3tuofza/upload", form)
+        const url2 = res.data.secure_url
+        await axios.patch(`http://127.0.0.1:3000/api/profile/cover/${id}`, {
+            url:url2
+        })
+        .then((res) => { setState(!state) })
+        .catch((err) => alert("failed to post blog"))
+    }
+
+
 
     return (
         <div>
-            <Profile user={user} />
+            <Profile user={user} changeCover={changeCover} changeProfile={changeProfile} setFile={setFile} change={change} setChange={setChange} updated={updated} setUpdated={setUpdated} />
+            <Photos />
             <Posts user={user} />
         </div>
     )
--- server/controller/profileController.js
+++ server/controller/profileController.js
@@ -1,80 +1,62 @@
 const Sequelize = require('sequelize');
-const {User , Product , Rating , ShoppingCart , Order} = require('../database-mysql/model')
+const { User, Product, Rating, ShoppingCart, Order } = require('../database-mysql/model')
 
-const getUser = async(req,res)=>{
-    try {
-      const user = await User.findByPk(req.params.id)
-      if(user){
-        res.status(200).send(user)
-      }
-    } catch (error) {
-        res.status(500).send(error)
+const getUser = async (req, res) => {
+  try {
+    const user = await User.findByPk(req.params.id)
+    if (user) {
+      res.status(200).send(user)
     }
+  } catch (error) {
+    res.status(500).send(error)
+  }
 }
 
 const updateCover = async (req, res) => {
-    try {
-      const [rowsAffected] = await User.update(
-        { coverUrl: req.body.url },
-        { where: { id: req.params.id } }
-      );
-      if (rowsAffected === 0) {
-        return res.status(404).send('User not found');
-      }
-      const updatedUser = await User.findByPk(req.params.id);
-      res.status(200).send(updatedUser);
-    } catch (error) {
-      res.status(500).send(error);
+  try {
+    const [rowsAffected] = await User.update(
+      { coverUrl: req.body.url },
+      { where: { id: req.params.id } }
+    );
+    if (rowsAffected === 0) {
+      return res.status(404).send('User not found');
     }
-  };
-  
-  const updateProfile = async (req, res) => {
-    try {
-      const [rowsAffected] = await User.update(
-        { profileUrl: req.body.url },
-        { where: { id: req.params.id } }
-      );
-      if (rowsAffected === 0) {
-        return res.status(404).send('User not found');
-      }
-      const updatedUser = await User.findByPk(req.params.id);
-      res.status(200).send(updatedUser);
-    } catch (error) {
-      res.status(500).send(error);
-    }
-  };
-
-
-  const editProfile = async (req, res) => {
-    if (req.body.username) {
-      try {
-        const [rowsAffected] = await User.update(
-          { username: req.body.username },
-          { where: { id: req.params.id } }
-        );
-        if (rowsAffected === 0) {
-          return res.status(404).send('User not found');
-        }
-        const updatedUser = await User.findByPk(req.params.id);
-        res.status(200).send(updatedUser);
-      } catch (error) {
-        res.status(500).send(error);
-      }
-    }
-    if(req.body.bio){
-      try {
-        const [rowsAffected2] = await User.update(
-          { bio: req.body.bio },
-          { where: { id: req.params.id } }
-        );
-        if (rowsAffected2 === 0) {
-          return res.status(404).send('User not found');
-        }
-        const updatedUser2 = await User.findByPk(req.params.id);
-        res.status(200).send(updatedUser2);
-      } catch (error) {
-        res.status(500).send(error);
-      }
-    }
+    const updatedUser = await User.findByPk(req.params.id);
+    res.status(200).send(updatedUser);
+  } catch (error) {
+    res.status(500).send(error);
   }
-module.exports = {getUser,updateCover,updateProfile,editProfile}
+};
+
+const updateProfile = async (req, res) => {
+  try {
+    const [rowsAffected] = await User.update(
+      { profileUrl: req.body.url },
+      { where: { id: req.params.id } }
+    );
+    if (rowsAffected === 0) {
+      return res.status(404).send('User not found');
+    }
+    const updatedUser = await User.findByPk(req.params.id);
+    res.status(200).send(updatedUser);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+
+const editProfile = async (req, res) => {
+  try {
+    if (req.body.username) {
+      await User.update({ username: req.body.username }, { where: { id: req.params.id } })
+    }
+    if (req.body.bio) {
+      await User.update({ bio: req.body.bio }, { where: { id: req.params.id } })
+    }
+    const updatedUser = await User.findByPk(req.params.id);
+    res.status(200).send(updatedUser);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+}
+module.exports = { getUser, updateCover, updateProfile, editProfile }
