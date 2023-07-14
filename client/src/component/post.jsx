62c26cdc4ab2aaf1e67173dc0fc177f186968053
MoNourTrabelsi <trabelsimednour@gmail.com>
2023-07-14 11:31:58 +0100

    friday

---
+++ client/src/component/post.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./profile.css"
+const Posts = ({user}) => {
+
+    return (
+        <div className="posts">
+            <div className="f-card">
+                <div className="header">
+                    <div className="options"><i className="fa fa-chevron-down"></i></div>
+                    <img className="co-logo" src={user.profileUrl} />
+                    <div className="co-name"><a href="#">{user.username}</a></div>
+                    <div className="time"><a href="#">2hrs</a> · <i className="fa fa-globe"></i></div>
+                </div>
+                <div className="content">
+                    <p>Height is optional, if no height is specified the image will be a square. Example: See More</p>
+                </div>
+
+                <div className="reference">
+                    <img className="reference-thumb" src="http://placehold.it/476x249" />
+                </div>
+            </div>
+        </div>
+    
+    )
+
+}
+
+export default Posts
